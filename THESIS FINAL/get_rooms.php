bf518fe7f1d5ff17fb4a81e1903113701d43f513
emmanuelle-altf4 <faith2miyuki@gmail.com>
2026-01-31 09:14:18 +0800

    editedsomething

--- THESIS FINAL/get_rooms.php
+++
@@ -1,7 +0,0 @@
-<?php
-// get_rooms.php
-$pdo = new PDO('mysql:host=localhost;dbname=resort', 'username', 'password');
-$stmt = $pdo->query("SELECT id, room_type, price FROM rooms WHERE available > 0");
-$rooms = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($rooms);
-?>
